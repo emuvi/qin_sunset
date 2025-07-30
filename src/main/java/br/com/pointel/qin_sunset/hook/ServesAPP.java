bc7b8ef540ffb081bb668928cc41da7fb2f27b1e
Éverton M. Vieira <emuvi@outlook.com.br>
2025-07-30 09:23:07 -0300

    refactor: reorder serves app

--- src/main/java/br/com/pointel/qin_sunset/hook/ServesAPP.java
+++ src/main/java/br/com/pointel/qin_sunset/hook/ServesAPP.java
@@ -14,8 +14,32 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class ServesApp {
     public static void init(ServletContextHandler context) {
-        initGet(context);
         initList(context);
+        initGet(context);
+    }
+
+    private static void initList(ServletContextHandler context) {
+        context.addServlet(new ServletHolder(new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                var wayToRun = Runner.getWayToRun(req);
+                if (wayToRun == null) {
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server does not have a way to run");
+                    return;
+                }
+                if (!Boolean.TRUE.equals(wayToRun.airWays.setup.servesApp)) {
+                    resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Server does not allow application access");
+                    return;
+                }
+                var authed = Runner.getAuthed(wayToRun, req);
+                if (authed == null) {
+                    resp.sendError(HttpServletResponse.SC_FORBIDDEN, "You must be logged");
+                    return;
+                }
+                resp.setContentType("text/plain");
+                resp.getWriter().print(OrdersApp.list(wayToRun, authed));
+            }
+        }), "/list/app");
     }
 
     private static void initGet(ServletContextHandler context) {
@@ -59,29 +83,5 @@ public class ServesApp {
                 OrdersApp.send(reqFile, resp);
             }
         }), "/app/*");
-    }
-
-    private static void initList(ServletContextHandler context) {
-        context.addServlet(new ServletHolder(new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                var wayToRun = Runner.getWayToRun(req);
-                if (wayToRun == null) {
-                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Server does not have a way to run");
-                    return;
-                }
-                if (!Boolean.TRUE.equals(wayToRun.airWays.setup.servesApp)) {
-                    resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Server does not allow application access");
-                    return;
-                }
-                var authed = Runner.getAuthed(wayToRun, req);
-                if (authed == null) {
-                    resp.sendError(HttpServletResponse.SC_FORBIDDEN, "You must be logged");
-                    return;
-                }
-                resp.setContentType("text/plain");
-                resp.getWriter().print(OrdersApp.list(wayToRun, authed));
-            }
-        }), "/list/app");
     }
 }
