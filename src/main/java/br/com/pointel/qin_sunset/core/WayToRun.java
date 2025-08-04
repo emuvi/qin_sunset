fb2db29ea27f251ccb4a23c29740268feb1fcd03
Éverton M. Vieira <emuvi@outlook.com.br>
2025-08-04 11:03:04 -0300

    fix: empty bases

--- src/main/java/br/com/pointel/qin_sunset/core/WayToRun.java
+++ src/main/java/br/com/pointel/qin_sunset/core/WayToRun.java
@@ -1,5 +1,6 @@
 package br.com.pointel.qin_sunset.core;
 
+import br.com.pointel.jarch.data.Bases;
 import br.com.pointel.jarch.data.Storage;
 
 public class WayToRun {
@@ -13,7 +14,7 @@ public class WayToRun {
         this.authedMap = new AuthedMap();
         this.stores = Boolean.TRUE.equals(airWays.setup.servesBas)
             ? new Storage(airWays.bases)
-            : new Storage();
+            : new Storage(new Bases());
     }
 
     public WayToRun(AirWays airWays, AuthedMap authedMap, Storage stores) {
